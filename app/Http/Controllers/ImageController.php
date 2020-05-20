7938c11bf68174f89023ae69cb6361bc367efd06
Nguyen_Hoang_Anh <hoanghuynh1998@gmail.com>
2020-05-20 12:28:31 +0700

    imagetopoint: test

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\AccountRequest;
+use Illuminate\Support\Facades\Validator;
+use Cloudder;
+
+class ImageController extends Controller
+{
+    public function getUrl()
+    {
+        return view('imagetopoint');
+    }
+
+    public function getImage(Requests $request)
+    {
+        $rules = [
+            'image' => 'required',
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if($validator->fails()){
+            return redirect()->back()->with('errors', "Image null!");
+        }
+
+        if ($request->hasFile('image')){
+            // create path to store in cloud
+            $public_id = "ninja_restaurant/imagetopoint/imagetopoint";
+            // upload to cloud
+            Cloudder::upload($request->file('image'), $public_id);
+            // get url of image
+            $resize = array("width" => 300, "height" => 300, "crop" => "fill");
+            $img_url = Cloudder::show($public_id, $resize);
+        }
+
+        return redirect()->back()->with('success', "Upload image successfully!");
+    }
+}
