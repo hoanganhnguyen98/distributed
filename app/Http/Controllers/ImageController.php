a1739977fd9975eb49a30a5f4165ceb26f4f8177
Nguyen_Hoang_Anh <hoanghuynh1998@gmail.com>
2020-05-20 15:19:44 +0700

    imagetopoint: set default image

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -12,10 +12,7 @@ class ImageController extends Controller
 {
     public function getUrl()
     {
-        $public_id = "ninja_restaurant/imagetopoint/imagetopoint";
-        // get url of image
-        $resize = array("width" => 500, "height" => 1000, "crop" => "fit");
-        $imageUrl = Cloudder::show($public_id, $resize);
+        $imageUrl = "https://res.cloudinary.com/ninjahh/image/upload/v1589960245/ninja_restaurant/imagetopoint/smzo5oyarcqru4nvao7d.png";
         return view('imagetopoint', compact('imageUrl'));
     }
 
