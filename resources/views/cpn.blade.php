95b6375e06d24f14db0804d0758e1329fc7c73d9
Nguyen_Hoang_Anh <hoanghuynh1998@gmail.com>
2020-09-04 21:52:22 +0700

    CPN: test

---
+++ resources/views/cpn.blade.php
@@ -0,0 +1,11 @@
+<form method="post" action=cpn-file enctype="multipart/form-data">
+    <div class="form-group">
+        <label>Tải File Excel</label>
+        <input type="file" name="file" class="form-control-file"
+        accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel" required>
+    </div>
+
+    <div class="form-group">
+        <button type="submit">Tải lên</button>
+    </div>
+</form>
