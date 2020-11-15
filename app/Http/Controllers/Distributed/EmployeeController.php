d0dd559e59748114ec615aa0fda7568fc4e25b54
Nguyen_Hoang_Anh <hoanghuynh1998@gmail.com>
2020-11-15 21:25:23 +0700

    dd check response in employee controller

--- app/Http/Controllers/Distributed/EmployeeController.php
+++ app/Http/Controllers/Distributed/EmployeeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Distributed;
 use App\Http\Controllers\Distributed\BaseController as BaseController;
 use Illuminate\Http\Request;
 use App\Model\Employee;
+use App\Model\Task;
 use Carbon\Carbon;
 
 class EmployeeController extends BaseController
@@ -15,11 +16,43 @@ class EmployeeController extends BaseController
 
     public function detail(Request $request)
     {
-        Employee::create([
-            'employee_id' => 100,
-            'name' => 'Nguyễn Văn X',
-            'current_id' => 1000,
-            'pending_ids' => ','
-        ]);
+        dd(1);
+        $employee_id = $request->get('id');
+
+        if (!$employee_id) {
+            return $this->sendError('Không có giá trị định danh nhân viên', 400);
+        }
+
+        $employee = Employee::where('employee_id', $employee_id)->first();
+
+        if (!$employee) {
+            return $this->sendError('Nhân viên chưa tham gia vào công việc nào', 400);
+        }
+
+        $current_id = $employee->current_id;
+        $current_task = Task::where([['id', $current_id], ['status', 'Đang xử lý']])->first();
+
+        $pending_ids = $employee->pending_ids;
+        $pending_tasks = [];
+
+        if (strlen($pending_ids) > 1) {
+            $pending_ids_array = array_slice(explode(',', $pending_ids, 1, -1));
+
+            foreach ($pending_ids_array as $id) {
+                $task = Task::where([['id', $id], ['status', 'Đang xử lý']])->first();
+
+                if ($task) {
+                    $pending_tasks[] = $task;
+                }
+            }
+        }
+
+        $data[] = [
+            'employee' => $employee,
+            'current_task' => $current_task,
+            'pending_tasks' => $pending_tasks
+        ];
+
+        return $this->sendResponse($data, 'Lấy thông tin nhân viên thành công');
     }
 }
