d6f4343b2c949aff6297525f69b39e15ae309698
Nguyen_Hoang_Anh <hoanghuynh1998@gmail.com>
2020-11-16 08:39:13 +0700

    add api to action with report

--- app/Http/Controllers/Distributed/ReportController.php
+++ app/Http/Controllers/Distributed/ReportController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Distributed;
 use App\Http\Controllers\Distributed\BaseController as BaseController;
 use Illuminate\Http\Request;
 use App\Model\Report;
+use App\Model\Task;
 use Carbon\Carbon;
+use App\Http\Controllers\Distributed\TaskController as TaskController;
 
 class ReportController extends BaseController
 {
@@ -42,5 +44,85 @@ class ReportController extends BaseController
         ];
 
         return $this->sendResponse($data);
+    }
+
+    public function accept(Request $request)
+    {
+        $id = $request->get('id');
+        $report = $this->isReportId($id);
+
+        $incident_id = $report->incident_id;
+        $task = Task::where('incident_id', $incident_id)->first();
+
+        if (!$task) {
+            return $this->sendError('Công việc xử lý của báo cáo không tồn tại', 400);
+        }
+
+        if ($task->status == 'done') {
+            $report->status = 'accept';
+            $report->save();
+
+            return $this->sendError('Công việc xử lý đã được xác nhận từ trước', 400);
+        }
+
+        if ($task->status == 'doing') {
+            $report->status = 'accept';
+            $report->save();
+
+            $task->status = 'done';
+            $task->save();
+
+            $task_id = $task->id;
+            $doing_employees = Employee::where('current_id', $task_id)->get();
+            $pending_employees = Employee::where('pending_ids', 'like', '%,'. $task_id . ',%')->get();
+
+            foreach ($pending_employees as $employee) {
+                // remove id from pending list
+                $pending_ids = $employee->pending_ids;
+                $pending_ids = str_replace($task_id . ',', '', $pending_ids);
+
+                $employee->pending_ids = $pending_ids;
+                $employee->save();
+
+                $this->notification('pending', 'remove', $employee->id);
+            }
+
+            foreach ($doing_employees as $employee) {
+                $employee->current_id = null;
+                $employee->save();
+
+                (new TaskController)->setCurrentTask($employee->id);
+            }
+
+            return $this->sendResponse();
+        }
+
+        return $this->sendError('Trạng thái của công việc xử lý không hợp lệ', 400);
+    }
+
+    public function reject(Request $request)
+    {
+        $id = $request->get('id');
+        $report = $this->isReportId($id);
+
+        $report->status = 'reject';
+        $report->save();
+
+        return $this->sendResponse();
+    }
+
+    public function isReportId($id)
+    {
+        if(!$id) {
+            return $this->sendError('Không có giá trị định danh báo cáo kết quả', 400);
+        }
+
+        $report = Report::where([['id', $id], ['status', 'waiting']])->first();
+
+        if (!$report) {
+            return $this->sendError('Định danh báo cáo kết quả không hợp lệ', 400);
+        }
+
+        return $report;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -28,6 +28,9 @@ Route::get('support/listing', 'Distributed\SupportController@listing');
 // bao cao
 Route::get('report/listing', 'Distributed\ReportController@listing');
 
+Route::post('report/accept', 'Distributed\ReportController@accept');
+Route::post('report/reject', 'Distributed\ReportController@reject');
+
 // nhan vien
 Route::get('employee/detail', 'Distributed\EmployeeController@detail');
 
